63a09d597d088d8a844de991663d8e5a00711a7e
Thiago MM <thimil@alunos.utfpr.edu.br>
2019-09-09 13:35:41 -0300

    mais coisa

--- listas.py
+++ listas.py
@@ -19,7 +19,8 @@ def parse_barreto(fisica):
             kk.append(dict(cap=line[0:line.find('<br>')],exs=list(map(lambda x: int(x), line[line.find('</b>:') + 5:].replace(',', ' ')
                                                 .replace(';', ' ')
                                                 .replace('<br>', ' ')
-                                                .replace('"', ' ').split()))))
+                                                .replace('"', ' ').split())),
+                           feitos=[]))
             pass
 
         # print(kk)
@@ -34,15 +35,54 @@ def recall_barreto():
     kk = json.load(jurek)
     print(kk)
 
+fisica = 2
+import os
+data = dict()
+if os.path.exists('data.json'):
+    aa = open('data.json')
+    data = json.load(aa)
+
 while True:
     print('q: quit')
     print('g: get data')
     print('p: print data')
+    print('e: exercise')
     user = input()
     if user == 'q':
+        jurek = open('data.json', 'w')
+        json.dump(data, jurek)
         exit(0)
     elif user == 'g':
         fis = input('numero da fisica: ')
         parse_barreto(fis)
+        jurek = open('kk.json')
+        data = json.load(jurek)
     elif user == 'p':
         recall_barreto()
+    elif user == 'e':
+        while True:
+            aaa = list(a for a in data)
+            for i, c in enumerate(list(a['cap'] for a in aaa)):
+                print(f'{i + 1}: {c}')
+            user = int(input()) - 1
+            if user < 0 or user >= len(aaa):
+                break
+            print('-----------')
+            print('-----------')
+            print('-----------')
+            print('-----------')
+            print(aaa[user]['cap'])
+            print('-----------')
+            print(aaa[user])
+            print('-----------')
+            afazer = list(k for k in aaa[user]['exs'] if k not in aaa[user]['feitos'])
+            print(afazer)
+            import random
+            ye = random.choice(afazer)
+            print(f'faça o ex {ye}')
+            print('-----------')
+            yn = input(f'fez o ex {ye}? ')
+            if yn not in ['0', 'n', 'N']:
+                aaa[user]['feitos'].append(ye)
+
+
